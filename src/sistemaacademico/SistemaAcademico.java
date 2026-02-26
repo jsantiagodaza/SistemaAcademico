54006b6887d64dd20d77a87c22b9baf999196ea9
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-02-26 16:32:04 -0500

    feat: Se ha creado la estructura Base del Main con los arraylist

--- src/sistemaacademico/SistemaAcademico.java
+++ src/sistemaacademico/SistemaAcademico.java
@@ -1,11 +1,43 @@
 
 package sistemaacademico;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SistemaAcademico {
 
   
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) { 
+        ArrayList<Asignatura> asignaturas = new ArrayList<>();
+        ArrayList<Estudiante> estudiantes = new ArrayList<>();
+        ArrayList<Nota> notas = new ArrayList<>();
+        
+        mostrarMenu();
+    }
+
+    public static void mostrarMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("_______________________________");
+            System.out.println("|            SISTEMA ACADEMICO             |");
+            System.out.println("_______________________________");
+
+            System.out.println("|1 --> Registrar Estudiante");
+            System.out.println("|2 --> Listar Estudiantes");
+            System.out.println("|3 --> Buscar Estudiante");
+            System.out.println("|4 --> Actualizar Estudiante");
+            System.out.println("|5 --> Eliminar Estudiante");
+            System.out.println("|6 --> Salir");
+
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+        } while (opcion != 0);
+    }
     }
     
-}
+
+
