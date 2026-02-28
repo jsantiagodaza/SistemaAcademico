d3c8cbfa0d09cbacc66697b276ef6bb31bcc88db
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-02-28 13:37:00 -0500

    chore(estudiante): agregar getters y setters de forma provisional

--- src/sistemaacademico/Estudiante.java
+++ src/sistemaacademico/Estudiante.java
@@ -1,5 +1,6 @@
 package sistemaacademico;
 //todo esto es base para poder probar un metodo, Xavi haz la clase correctamente, -santiago
+//pendiente y a la espera del Dev1, 
 public class Estudiante {
     private int id;
     private String nombre;
@@ -10,4 +11,36 @@ public class Estudiante {
         this.nombre = nombre;
         this.edad = edad;
     }
+    //getters que necesito para trabajar la crud 
+     public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    //setters necesarios para actualizar estudiante, dev1 verificalos y desarrolla esta clase
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    //override para impresión clara
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               " | Nombre: " + nombre +
+               " | Edad: " + edad;
+    }
 }
+
