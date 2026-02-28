75f95cd464124faac66b346accde37b9d8a07789
Xavi Jesus Acosta Maldonado <xacosta@unicesar.educ.co>
2026-02-28 17:30:31 -0500

    feat: se agrego la clase estudiante()

--- src/sistemaacademico/Estudiante.java
+++ src/sistemaacademico/Estudiante.java
@@ -1,13 +1,77 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package sistemaacademico;
 
-/**
- *
- * @author santi
- */
+
 public class Estudiante {
+    
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int semestre;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String codigo, String nombre, String apellido, int edad, int semestre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.semestre = semestre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+ 
+   public String toString(){
+     
+       return "Estudiante{"+
+               "codigo:" + codigo +
+               ", Nombre:" + nombre + "\n " +
+               ", Apellido: " + apellido + "\n " + 
+               ", Edad: " + edad + "\n " +
+               ", Semestre: " + semestre + "\n ";
+       
+   }
+    
+    
     
 }
